eb12ae9fde10bbe0e90b16d07f81878d305a1dda
Amit <none@none>
2015-11-30 10:43:48 +0530

    Hyst interface embedded with XSpeed for Inputs to model/config files. Incremental build not included

---
+++ Hybrid_Model_Parameters_Design/user_model/user_model.h
@@ -0,0 +1,27 @@
+/*
+ * user_model.h
+ *
+ *  Created on: 26-Nov-2015
+ *      Author: amit
+ */
+
+#ifndef USER_MODEL_H_
+#define USER_MODEL_H_
+
+
+#include "core_system/continuous/Polytope/Polytope.h"
+#include "core_system/HybridAutomata/Hybrid_Automata.h"
+#include "core_system/symbolic_states/symbolic_states.h"
+#include "core_system/math/matrix.h"
+#include "application/All_PP_Definition.h"
+#include "core_system/math/uni_sphere.h"	//for obtaining uniformly distributed directions
+#include "application/sf_directions.h"
+#include "application/sf_utility.h"
+//#include <boost/numeric/ublas/matrix.hpp>
+
+void user_model(hybrid_automata& Hybrid_Automata,
+		symbolic_states& initial_symbolic_states,
+		ReachabilityParameters& reach_parameters, int& transition_iterations);
+
+
+#endif /* USER_MODEL_H_ */
---
+++ application/plotter_utility.h
@@ -0,0 +1,17 @@
+/*
+ * plotter_utility.h
+ *
+ *  Created on: 18-Nov-2015
+ *      Author: amit
+ */
+
+#ifndef PLOTTER_UTILITY_H_
+#define PLOTTER_UTILITY_H_
+
+#include "Utilities/Template_Polyhedra.h"
+#include "boost/lexical_cast.hpp"
+
+void plot_output(std::list<template_polyhedra> reachability_sfm);
+
+
+#endif /* PLOTTER_UTILITY_H_ */
