b93bbae0d25b5842d888c2aee14e11f1e8e3e1be
Rajarshi <none@none>
2017-04-10 10:16:31 +0530

    motorcar.h added

---
+++ Hybrid_Model_Parameters_Design/test_models/Arch_Benchmark/motorcar.h
@@ -0,0 +1,45 @@
+/*
+ * motorcar.h
+ *
+ *  Created on: 09-Apr-2017
+ *      Author: rajarshi
+ */
+
+#ifndef HYBRID_MODEL_PARAMETERS_DESIGN_TEST_MODELS_ARCH_BENCHMARK_MOTORCAR_H_
+#define HYBRID_MODEL_PARAMETERS_DESIGN_TEST_MODELS_ARCH_BENCHMARK_MOTORCAR_H_
+
+#include "core_system/continuous/Polytope/Polytope.h"
+#include "core_system/HybridAutomata/Hybrid_Automata.h"
+#include "core_system/symbolic_states/symbolic_states.h"
+#include "core_system/symbolic_states/initial_state.h"
+#include "core_system/math/matrix.h"
+#include "application/All_PP_Definition.h"
+#include "core_system/math/uni_sphere.h"	//for obtaining uniformly distributed directions
+#include "application/sf_directions.h"
+#include "application/sf_utility.h"
+#include "application/userOptions.h"
+
+
+//void user_model(hybrid_automata& Hybrid_Automata,initial_state::ptr& init_state,ReachabilityParameters& reach_parameters);
+
+void module1(hybrid_automata& ha,
+		std::list<initial_state::ptr>& init_state,
+		ReachabilityParameters& reach_parameters,
+		userOptions& op);
+void module2(hybrid_automata& ha,
+		std::list<initial_state::ptr>& init_state,
+		ReachabilityParameters& reach_parameters,
+		userOptions& op);
+void module3(hybrid_automata& ha,
+		std::list<initial_state::ptr>& init_state,
+		ReachabilityParameters& reach_parameters,
+		userOptions& op);
+void module4(hybrid_automata& ha,
+		std::list<initial_state::ptr>& init_state,
+		ReachabilityParameters& reach_parameters,
+		userOptions& op);
+
+void setMotorcar(hybrid_automata& Hybrid_Automata, std::list<initial_state::ptr>& init_state_list, ReachabilityParameters& reach_parameters, userOptions& op);
+
+
+#endif /* HYBRID_MODEL_PARAMETERS_DESIGN_TEST_MODELS_ARCH_BENCHMARK_MOTORCAR_H_ */
