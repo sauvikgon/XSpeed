e0d8386d91bf8468e457f2538a3eb463b5cfdc61
Rajarshi <none@none>
2017-10-21 18:41:16 +0530

    new tester,utility files added

---
+++ testing/Unit_Testing_Math/test_hausdorffDist.cpp
@@ -0,0 +1,70 @@
+/*
+ * test_hausdorffDist.cpp
+ *
+ *  Created on: 19-Sep-2017
+ *      Author: rajarshi
+ */
+
+#include "UnitTest++.h" //manual installation and copy in /usr/local/include/UnitTest++ folder
+#include "core_system/math/matrix.h"
+#include <iostream>
+#include <sstream>
+#include <exception>
+#include <vector>
+#include <cmath>
+#include "Utilities/HausdorffDistance.h"
+#include "core_system/continuous/Polytope/Polytope.h"
+
+
+using namespace std;
+
+SUITE(HausdorffDistanceTestSuite) {
+struct Example {
+	Example() {
+	}
+
+	stringstream expected, test;
+	~Example() { /* some teardown */
+	}
+
+};
+
+TEST_FIXTURE(Example, HausdorffDistanceTest) {
+
+	polytope::ptr I, P2;
+	typedef typename boost::numeric::ublas::matrix<double>::size_type size_type;
+	size_type row, col;
+	int boundSignV;
+	std::vector<double> boundValueV;
+	math::matrix<double> ConstraintsMatrixV;
+
+	polytope::ptr G;
+	row = 4;
+	col = 2;
+	ConstraintsMatrixV.resize(row, col);
+	ConstraintsMatrixV(0, 0) = 0;
+	ConstraintsMatrixV(0, 1) = 1;
+	ConstraintsMatrixV(1, 0) = 0;
+	ConstraintsMatrixV(1, 1) = -1;
+	ConstraintsMatrixV(2, 0) = 1;
+	ConstraintsMatrixV(2, 1) = 0;
+	ConstraintsMatrixV(3, 0) = -1;
+	ConstraintsMatrixV(3, 1) = 0;
+
+	boundValueV.resize(4);
+	boundValueV[0] = 15;
+	boundValueV[1] = -5;
+	boundValueV[2] = 15;//9.8;
+	boundValueV[3] = -5;//-9.8;
+
+	boundSignV = 1;
+
+	G = polytope::ptr(new polytope(ConstraintsMatrixV, boundValueV, boundSignV));
+
+	double d = HausdorffDistance(G,G);
+	std::cout << "Hausdorff Distance: " << d << std::endl;
+	CHECK_EQUAL(d,0);
+
+}
+
+}
