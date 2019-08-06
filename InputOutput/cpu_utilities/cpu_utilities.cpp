36de87ffc42d49ee94a7bb24ce9242f6658d5d6b
Amrita <none@none>
2019-08-06 23:14:28 +0530

    CPU utilities updated to support MacOS

--- InputOutput/cpu_utilities/cpu_utilities.cpp
+++ InputOutput/cpu_utilities/cpu_utilities.cpp
@@ -6,12 +6,17 @@
  */
 
 #include "cpu_utilities.h"
-
+#include  <iostream>
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 #include "sys/times.h"
+#include <sys/sysctl.h>
+#include <sys/param.h>
+#include <unistd.h>
+#include <sys/types.h>
 //#include "sys/vtimes.h"
+using namespace std;
 
 static clock_t lastCPU, lastSysCPU, lastUserCPU;
 static int numProcessors;
@@ -19,17 +24,26 @@ static int numProcessors;
 void init_cpu_usage() {
 	FILE* file;
 	struct tms timeSample;
-	char line[128];
+	char line[10000];
 
 	lastCPU = times(&timeSample);
 	lastSysCPU = timeSample.tms_stime;
 	lastUserCPU = timeSample.tms_utime;
-
+	cout << "\nMemory and CPU stats can only be calculated for Linux or MacOS distributions. Other OS may cause segmentation fault.\n";
 	file = fopen("/proc/cpuinfo", "r");
 	numProcessors = 0;
-	while (fgets(line, 128, file) != NULL) {
+	if(file==NULL)
+	{//The OS is MacOS,
+		size_t numProcessors_size=sizeof(numProcessors);
+		if(sysctlbyname("hw.logicalcpu", &numProcessors, &numProcessors_size, nullptr, 0)!=0)
+				numProcessors--;
+	}
+	else{
+		// OS IS UBUNTU
+		while (fgets(line, 128, file)!= NULL) {
 		if (strncmp(line, "processor", 9) == 0)
 			numProcessors++;
+		}
 	}
 	fclose(file);
 }
@@ -44,7 +58,8 @@ double getCurrent_ProcessCPU_usage() {
 			|| timeSample.tms_utime < lastUserCPU) {
 		//Overflow detection. Just skip this value.
 		percent = -1.0;
-	} else {
+	}
+	else {
 		percent = (timeSample.tms_stime - lastSysCPU)
 				+ (timeSample.tms_utime - lastUserCPU);
 		percent /= (now - lastCPU);
@@ -56,4 +71,5 @@ double getCurrent_ProcessCPU_usage() {
 	lastUserCPU = timeSample.tms_utime;
 
 	return percent;
+//	std::cout << "\nCpu percentage: \n" << percent;
 }
